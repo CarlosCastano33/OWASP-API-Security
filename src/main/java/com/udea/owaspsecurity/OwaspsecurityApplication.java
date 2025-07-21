30697db906939f34d040b8c5a787ca8cd9994939
Carlos Castaño <carlose.castano@udea.edu.co>
2025-07-21 10:43:41 -0500

    Creación

---
+++ src/main/java/com/udea/owaspsecurity/OwaspsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.udea.owaspsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwaspsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwaspsecurityApplication.class, args);
+	}
+
+}
